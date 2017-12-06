3ba2e41d927b15043c012f8cb0bc3bcf4d90a3c8
Chris Dawson <cdawson@opensource.cirrus.com>
2017-12-06 14:47:45 +0000

    ASoC: clsic: Patch due to change in tacna_init_fll function prototype
    The fll data structure has changed, which also changes the prototype
    for tacna_init_fll.
    
    Change-Id: Iee91a4b1c2aab79057ed70ea5040efbe696e2582
    Signed-off-by: Chris Dawson <cdawson@opensource.cirrus.com>

--- sound/soc/codecs/clsic-codec.c
+++ sound/soc/codecs/clsic-codec.c
@@ -1907,7 +1907,7 @@ static int clsic_probe(struct platform_device *pdev)
 		dev_err(&pdev->dev, "Failed to initialise DSP2.\n");
 
 	for (i = 0 ; i < CLSIC_N_FLL ; ++i)
-		tacna_init_fll(clsic_codec->core.tacna,
+		tacna_init_fll(&clsic_codec->core,
 			       i + 1,
 			       TACNA_FLL1_CONTROL1 + i * 0x100,
 			       CLSIC_IRQ2_STS6,
