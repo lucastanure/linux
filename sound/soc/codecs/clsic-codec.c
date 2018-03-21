dde85f64c5ed43346914c8f6589d7e9c3685834b
Simon Trimmer <simont@opensource.cirrus.com>
2018-03-21 14:21:56 +0000

    ASoC: clsic-codec: async rates now held in tacna_sample_rate_async[]
    Change-Id: I7daa6188f20a0c514cad92c02eee3de7f66e7134
    Signed-off-by: Simon Trimmer <simont@opensource.cirrus.com>

--- sound/soc/codecs/clsic-codec.c
+++ sound/soc/codecs/clsic-codec.c
@@ -289,8 +289,8 @@ SOC_ENUM("LHPF4 Mode", tacna_lhpf4_mode),
 
 SOC_ENUM("Sample Rate 2", tacna_sample_rate[0]),
 SOC_ENUM("Sample Rate 3", tacna_sample_rate[1]),
-SOC_ENUM("Async Sample Rate 1", tacna_sample_rate[2]),
-SOC_ENUM("Async Sample Rate 2", tacna_sample_rate[3]),
+SOC_ENUM("Async Sample Rate 1", tacna_sample_rate_async[0]),
+SOC_ENUM("Async Sample Rate 2", tacna_sample_rate_async[1]),
 
 TACNA_RATE_ENUM("FX Rate", tacna_fx_rate),
 
