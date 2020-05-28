CirrusLogic/linux-drivers

ASoC: cs47l96: Write IN_VU bit after volume or mute changes
A 1 has to be written to IN_VU after any changes to the mute
or volume.

The register containing IN_VU is write-only and only ever needs
a 1 written to it. It is marked volatile so that it will be
uncached, allowing repeated writes of the same value.

For efficiency the input event widget saves the IN_VU until
after all the inputs being activated have been unmuted so
only a single write is needed. For disables the IN_VU is
applied immediately because we want to mute before disabling
to prevent pops.

For the volume controls the write to IN_VU is allowed to fail
if the codec is powered down, since it's irrelevant until the
input is enabled and the power-up of the widget will write
to IN_VU.

Change-Id: I339a984c09d33e1fa53fc7345a0f1d66431d2f0e
Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

sound/soc/codecs/cs47l96-ao.c
@@ -217,18 +217,30 @@ SOC_SINGLE("IN7LAO HPF Switch", TACNA_IN7LAO_CONTROL1, TACNA_IN7LAO_HPF_SHIFT,
 SOC_SINGLE("IN7RAO HPF Switch", TACNA_IN7RAO_CONTROL1, TACNA_IN7RAO_HPF_SHIFT,
 	   1, 0),
 
-SOC_SINGLE_TLV("IN5LAO Digital Volume", TACNA_IN5L_CONTROL2,
-	       TACNA_IN5LAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN5RAO Digital Volume", TACNA_IN5R_CONTROL2,
-	       TACNA_IN5RAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN6LAO Digital Volume", TACNA_IN6L_CONTROL2,
-	       TACNA_IN6LAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN6RAO Digital Volume", TACNA_IN6R_CONTROL2,
-	       TACNA_IN6RAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN7LAO Digital Volume", TACNA_IN7L_CONTROL2,
-	       TACNA_IN7LAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN7RAO Digital Volume", TACNA_IN7R_CONTROL2,
-	       TACNA_IN7RAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN5LAO Digital Volume", TACNA_IN5L_CONTROL2,
+		   TACNA_IN5LAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN5RAO Digital Volume", TACNA_IN5R_CONTROL2,
+		   TACNA_IN5RAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN6LAO Digital Volume", TACNA_IN6L_CONTROL2,
+		   TACNA_IN6LAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN6RAO Digital Volume", TACNA_IN6R_CONTROL2,
+		   TACNA_IN6RAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN7LAO Digital Volume", TACNA_IN7L_CONTROL2,
+		   TACNA_IN7LAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN7RAO Digital Volume", TACNA_IN7R_CONTROL2,
+		   TACNA_IN7RAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
 
 SOC_ENUM("AO Input Ramp Up", cs47l96_ao_in_vi_ramp),
 SOC_ENUM("AO Input Ramp Down", cs47l96_ao_in_vd_ramp),
@@ -348,15 +360,20 @@ static int cs47l96_ao_in_ev(struct snd_soc_dapm_widget *w,
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
-		priv->in_pending++;
+		priv->in_up_pending++;
 		break;
 	case SND_SOC_DAPM_POST_PMU:
-		priv->in_pending--;
+		priv->in_up_pending--;
 		snd_soc_update_bits(codec, reg, TACNA_IN1LAO_MUTE, 0);
+
+		/* Uncached write-only register, no need for update_bits */
+		if (!priv->in_up_pending)
+			snd_soc_write(codec, priv->in_vu_reg, TACNA_IN_VU);
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
 		snd_soc_update_bits(codec, reg,
 				    TACNA_IN1LAO_MUTE, TACNA_IN1LAO_MUTE);
+		snd_soc_write(codec, priv->in_vu_reg, TACNA_IN_VU);
 		break;
 	default:
 		break;
@@ -777,6 +794,7 @@ static int cs47l96_ao_probe(struct platform_device *pdev)
 	cs47l96_ao->core.dev = &pdev->dev;
 	cs47l96_ao->core.num_inputs = 6;
 	cs47l96_ao->core.max_analogue_inputs = 0;
+	cs47l96_ao->core.in_vu_reg = TACNA_AO_INPUT_CONTROL3;
 
 	mutex_init(&cs47l96_ao->core.rate_lock);
 
@@ -819,10 +837,6 @@ static int cs47l96_ao_probe(struct platform_device *pdev)
 
 	for (i = 0; i < ARRAY_SIZE(cs47l96_ao_dai); i++)
 		tacna_init_dai(&cs47l96_ao->core, i);
-
-	/* Latch volume update bits */
-	regmap_update_bits(tacna->regmap, TACNA_AO_INPUT_CONTROL3,
-			   TACNA_AO_IN_VU_MASK, TACNA_AO_IN_VU);
 
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_idle(&pdev->dev);

sound/soc/codecs/tacna.c
@@ -3472,15 +3472,20 @@ int tacna_in_ev(struct snd_soc_dapm_widget *w, struct snd_kcontrol *kcontrol,
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
-		priv->in_pending++;
+		priv->in_up_pending++;
 		break;
 	case SND_SOC_DAPM_POST_PMU:
-		priv->in_pending--;
+		priv->in_up_pending--;
 		snd_soc_update_bits(codec, reg, TACNA_IN1L_MUTE, 0);
+
+		/* Uncached write-only register, no need for update_bits */
+		if (!priv->in_up_pending && priv->in_vu_reg)
+			snd_soc_write(codec, priv->in_vu_reg, TACNA_IN_VU);
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
 		snd_soc_update_bits(codec, reg,
 				    TACNA_IN1L_MUTE, TACNA_IN1L_MUTE);
+		snd_soc_write(codec, priv->in_vu_reg, TACNA_IN_VU);
 		break;
 	default:
 		break;
@@ -3489,6 +3494,28 @@ int tacna_in_ev(struct snd_soc_dapm_widget *w, struct snd_kcontrol *kcontrol,
 	return 0;
 }
 EXPORT_SYMBOL_GPL(tacna_in_ev);
+
+int tacna_in_put_volsw(struct snd_kcontrol *kcontrol,
+		       struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_codec *codec = snd_soc_kcontrol_codec(kcontrol);
+	struct tacna_priv *priv = snd_soc_codec_get_drvdata(codec);
+	int ret;
+
+	ret = snd_soc_put_volsw(kcontrol, ucontrol);
+	if (ret < 0)
+		return ret;
+
+	/*
+	 * Uncached write-only register, no need for update_bits.
+	 * Will fail if codec is off but that will be handled by tacna_in_ev
+	 */
+	if (priv->in_vu_reg)
+		snd_soc_write(codec, priv->in_vu_reg, TACNA_IN_VU);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(tacna_in_put_volsw);
 
 int tacna_wait_for_output_seq(struct tacna_priv *priv, unsigned int mask,
 			      unsigned int target)

lucastanure/linux

sound/soc/codecs/cs47l96-ao.c
@@ -219,18 +219,30 @@ SOC_SINGLE("IN7LAO HPF Switch", TACNA_IN7LAO_CONTROL1, TACNA_IN7LAO_HPF_SHIFT,
 SOC_SINGLE("IN7RAO HPF Switch", TACNA_IN7RAO_CONTROL1, TACNA_IN7RAO_HPF_SHIFT,
 	   1, 0),
 
-SOC_SINGLE_TLV("IN5LAO Digital Volume", TACNA_IN5L_CONTROL2,
-	       TACNA_IN5LAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN5RAO Digital Volume", TACNA_IN5R_CONTROL2,
-	       TACNA_IN5RAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN6LAO Digital Volume", TACNA_IN6L_CONTROL2,
-	       TACNA_IN6LAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN6RAO Digital Volume", TACNA_IN6R_CONTROL2,
-	       TACNA_IN6RAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN7LAO Digital Volume", TACNA_IN7L_CONTROL2,
-	       TACNA_IN7LAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
-SOC_SINGLE_TLV("IN7RAO Digital Volume", TACNA_IN7R_CONTROL2,
-	       TACNA_IN7RAO_VOL_SHIFT, 0xbf, 0, tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN5LAO Digital Volume", TACNA_IN5L_CONTROL2,
+		   TACNA_IN5LAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN5RAO Digital Volume", TACNA_IN5R_CONTROL2,
+		   TACNA_IN5RAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN6LAO Digital Volume", TACNA_IN6L_CONTROL2,
+		   TACNA_IN6LAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN6RAO Digital Volume", TACNA_IN6R_CONTROL2,
+		   TACNA_IN6RAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN7LAO Digital Volume", TACNA_IN7L_CONTROL2,
+		   TACNA_IN7LAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
+SOC_SINGLE_EXT_TLV("IN7RAO Digital Volume", TACNA_IN7R_CONTROL2,
+		   TACNA_IN7RAO_VOL_SHIFT, 0xbf, 0,
+		   snd_soc_get_volsw, tacna_in_put_volsw,
+		   tacna_digital_tlv),
 
 SOC_ENUM("AO Input Ramp Up", cs47l96_ao_in_vi_ramp),
 SOC_ENUM("AO Input Ramp Down", cs47l96_ao_in_vd_ramp),
@@ -350,15 +362,21 @@ static int cs47l96_ao_in_ev(struct snd_soc_dapm_widget *w,
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
-		priv->in_pending++;
+		priv->in_up_pending++;
 		break;
 	case SND_SOC_DAPM_POST_PMU:
-		priv->in_pending--;
+		priv->in_up_pending--;
 		snd_soc_component_update_bits(comp, reg, TACNA_IN1LAO_MUTE, 0);
+
+		/* Uncached write-only register, no need for update_bits */
+		if (!priv->in_up_pending)
+			snd_soc_component_write(comp, priv->in_vu_reg,
+						TACNA_IN_VU);
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
 		snd_soc_component_update_bits(comp, reg, TACNA_IN1LAO_MUTE,
 					      TACNA_IN1LAO_MUTE);
+		snd_soc_component_write(comp, priv->in_vu_reg, TACNA_IN_VU);
 		break;
 	default:
 		break;
@@ -767,6 +785,7 @@ static int cs47l96_ao_probe(struct platform_device *pdev)
 	cs47l96_ao->core.dev = &pdev->dev;
 	cs47l96_ao->core.num_inputs = 6;
 	cs47l96_ao->core.max_analogue_inputs = 0;
+	cs47l96_ao->core.in_vu_reg = TACNA_AO_INPUT_CONTROL3;
 
 	mutex_init(&cs47l96_ao->core.rate_lock);
 
@@ -809,10 +828,6 @@ static int cs47l96_ao_probe(struct platform_device *pdev)
 
 	for (i = 0; i < ARRAY_SIZE(cs47l96_ao_dai); i++)
 		tacna_init_dai(&cs47l96_ao->core, i);
-
-	/* Latch volume update bits */
-	regmap_update_bits(tacna->regmap, TACNA_AO_INPUT_CONTROL3,
-			   TACNA_AO_IN_VU_MASK, TACNA_AO_IN_VU);
 
 	pm_runtime_enable(&pdev->dev);
 	pm_runtime_idle(&pdev->dev);

sound/soc/codecs/tacna.c
@@ -3473,15 +3473,21 @@ int tacna_in_ev(struct snd_soc_dapm_widget *w, struct snd_kcontrol *kcontrol,
 
 	switch (event) {
 	case SND_SOC_DAPM_PRE_PMU:
-		priv->in_pending++;
+		priv->in_up_pending++;
 		break;
 	case SND_SOC_DAPM_POST_PMU:
-		priv->in_pending--;
+		priv->in_up_pending--;
 		snd_soc_component_update_bits(comp, reg, TACNA_IN1L_MUTE, 0);
+
+		/* Uncached write-only register, no need for update_bits */
+		if (!priv->in_up_pending && priv->in_vu_reg)
+			snd_soc_component_write(comp, priv->in_vu_reg,
+						TACNA_IN_VU);
 		break;
 	case SND_SOC_DAPM_PRE_PMD:
 		snd_soc_component_update_bits(comp, reg,
 				    TACNA_IN1L_MUTE, TACNA_IN1L_MUTE);
+		snd_soc_component_write(comp, priv->in_vu_reg, TACNA_IN_VU);
 		break;
 	default:
 		break;
@@ -3490,6 +3496,28 @@ int tacna_in_ev(struct snd_soc_dapm_widget *w, struct snd_kcontrol *kcontrol,
 	return 0;
 }
 EXPORT_SYMBOL_GPL(tacna_in_ev);
+
+int tacna_in_put_volsw(struct snd_kcontrol *kcontrol,
+		       struct snd_ctl_elem_value *ucontrol)
+{
+	struct snd_soc_component *comp = snd_soc_kcontrol_component(kcontrol);
+	struct tacna_priv *priv = snd_soc_component_get_drvdata(comp);
+	int ret;
+
+	ret = snd_soc_put_volsw(kcontrol, ucontrol);
+	if (ret < 0)
+		return ret;
+
+	/*
+	 * Uncached write-only register, no need for update_bits.
+	 * Will fail if component is off but that will be handled by tacna_in_ev
+	 */
+	if (priv->in_vu_reg)
+		snd_soc_component_write(comp, priv->in_vu_reg, TACNA_IN_VU);
+
+	return 0;
+}
+EXPORT_SYMBOL_GPL(tacna_in_put_volsw);
 
 int tacna_wait_for_output_seq(struct tacna_priv *priv, unsigned int mask,
 			      unsigned int target)