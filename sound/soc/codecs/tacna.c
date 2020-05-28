CirrusLogic/linux-drivers

ASoC: tacna: Enable tacna_get_accdet_for_output
Now there is an extcon driver this function must be enabled so that
the clamp and short circuit checks work.

Change-Id: I2cd913f7e06ebedbe0d21de72748f1313d3e2e07
Signed-off-by: Richard Fitzgerald <rf@opensource.wolfsonmicro.com>

sound/soc/codecs/tacna.c
@@ -3450,13 +3450,13 @@ EXPORT_SYMBOL_GPL(tacna_set_output_mode);
 
 int tacna_get_accdet_for_output(struct snd_soc_codec *codec, int output)
 {
-/*	struct tacna *tacna = dev_get_drvdata(codec->dev->parent);
+	struct tacna *tacna = dev_get_drvdata(codec->dev->parent);
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(tacna->pdata.accdet); i++) {
+	for (i = 0; i < ARRAY_SIZE(tacna->pdata.accdet); i++)
 		if (tacna->pdata.accdet[i].output == output)
 			return i;
-*/
+
 	return -ENODEV;
 }
 EXPORT_SYMBOL_GPL(tacna_get_accdet_for_output);

lucastanure/linux

sound/soc/codecs/tacna.c
@@ -3450,13 +3450,13 @@ EXPORT_SYMBOL_GPL(tacna_set_output_mode);
 
 int tacna_get_accdet_for_output(struct snd_soc_component *comp, int output)
 {
-/*	struct tacna *tacna = dev_get_drvdata(comp->dev->parent);
+	struct tacna *tacna = dev_get_drvdata(comp->dev->parent);
 	int i;
 
-	for (i = 0; i < ARRAY_SIZE(tacna->pdata.accdet); i++) {
+	for (i = 0; i < ARRAY_SIZE(tacna->pdata.accdet); i++)
 		if (tacna->pdata.accdet[i].output == output)
 			return i;
-*/
+
 	return -ENODEV;
 }
 EXPORT_SYMBOL_GPL(tacna_get_accdet_for_output);