CirrusLogic/linux-drivers

ASoC: tacna: handle interaction with accessory detection
Change-Id: I8ce7df5c336a08cb7eee601cc53ca17ab7f14703
Signed-off-by: Piotr Stankiewicz <piotrs@opensource.wolfsonmicro.com>

sound/soc/codecs/tacna.c
@@ -3634,7 +3634,7 @@ int tacna_hp_ev(struct snd_soc_dapm_widget *w,
 	struct tacna *tacna = priv->tacna;
 	unsigned int mask = 1 << w->shift;
 	unsigned int val;
-	int ret;
+	int ret, accdet;
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
@@ -3650,9 +3650,16 @@ int tacna_hp_ev(struct snd_soc_dapm_widget *w,
 		return 0;
 	}
 
+	/* store desired output state */
+	tacna->hp_ena = (tacna->hp_ena & ~mask) | val;
+
 	/*
-	 * TODO: handle interaction with accessory detection
+	 * disable output, if clamp is active (output state will be applied when
+	 * the clamp is disabled)
 	 */
+	accdet = tacna_get_accdet_for_output(codec, (mask / 2) + 1);
+	if (accdet >= 0 && tacna->hpdet_clamp[accdet])
+		val = 0;
 
 	ret = regmap_update_bits(tacna->regmap, TACNA_OUTPUT_ENABLE_1,
 				 mask, val);

lucastanure/linux

sound/soc/codecs/tacna.c
@@ -3635,7 +3635,7 @@ int tacna_hp_ev(struct snd_soc_dapm_widget *w,
 	struct tacna *tacna = priv->tacna;
 	unsigned int mask = 1 << w->shift;
 	unsigned int val;
-	int ret;
+	int ret, accdet;
 
 	switch (event) {
 	case SND_SOC_DAPM_POST_PMU:
@@ -3651,9 +3651,16 @@ int tacna_hp_ev(struct snd_soc_dapm_widget *w,
 		return 0;
 	}
 
+	/* store desired output state */
+	tacna->hp_ena = (tacna->hp_ena & ~mask) | val;
+
 	/*
-	 * TODO: handle interaction with accessory detection
+	 * disable output, if clamp is active (output state will be applied when
+	 * the clamp is disabled)
 	 */
+	accdet = tacna_get_accdet_for_output(comp, (mask / 2) + 1);
+	if (accdet >= 0 && tacna->hpdet_clamp[accdet])
+		val = 0;
 
 	ret = regmap_update_bits(tacna->regmap, TACNA_OUTPUT_ENABLE_1,
 				 mask, val);