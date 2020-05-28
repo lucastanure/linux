CirrusLogic/linux-drivers

ASoC: cs47l96: Use snd_soc_component_disable_pin
The snd_soc_component_disable_pin function automatically
prepends the codec name prefix.

Change-Id: I67294b5457c1c334cabfc6a324e328395743f500
Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

sound/soc/codecs/cs47l96.c
@@ -2635,6 +2635,8 @@ static irqreturn_t cs47l96_dsp1_irq(int irq, void *data)
 static int cs47l96_codec_probe(struct snd_soc_codec *codec)
 {
 	struct cs47l96 *cs47l96 = snd_soc_codec_get_drvdata(codec);
+	struct snd_soc_dapm_context *dapm = snd_soc_codec_get_dapm(codec);
+	struct snd_soc_component *component = snd_soc_dapm_to_component(dapm);
 	struct tacna *tacna = cs47l96->core.tacna;
 	int ret;
 
@@ -2661,7 +2663,7 @@ static int cs47l96_codec_probe(struct snd_soc_codec *codec)
 	if (ret)
 		return ret;
 
-	snd_soc_dapm_disable_pin(tacna->dapm, "HAPTICS");
+	snd_soc_component_disable_pin(component, "HAPTICS");
 
 	ret = tacna_dsp_add_codec_controls(codec, CS47L96_NUM_DSP);
 	if (ret)

lucastanure/linux

sound/soc/codecs/cs47l96.c
@@ -2629,7 +2629,7 @@ static int cs47l96_component_probe(struct snd_soc_component *comp)
 	if (ret)
 		return ret;
 
-	snd_soc_dapm_disable_pin(tacna->dapm, "HAPTICS");
+	snd_soc_component_disable_pin(comp, "HAPTICS");
 
 	ret = tacna_dsp_add_component_controls(comp, CS47L96_NUM_DSP);
 	if (ret)