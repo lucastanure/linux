CirrusLogic/linux-drivers

ASoC: cs48l32: Add & when getting address of function
Satisfies strict code checking tools.

Change-Id: I3c6f50a6e1212c16cbb3797aa1657832569cec8b
Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

sound/soc/codecs/cs48l32.c
@@ -1209,7 +1209,7 @@ static struct snd_soc_dai_driver cs48l32_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs48l32-dsp-trace",
@@ -1230,7 +1230,7 @@ static struct snd_soc_dai_driver cs48l32_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs48l32-dsp-voicectrl",
@@ -1339,14 +1339,14 @@ static struct regmap *cs48l32_get_regmap(struct device *dev)
 }
 
 static struct snd_soc_codec_driver soc_codec_dev_cs48l32 = {
-	.probe = cs48l32_codec_probe,
-	.remove = cs48l32_codec_remove,
-	.get_regmap = cs48l32_get_regmap,
+	.probe = &cs48l32_codec_probe,
+	.remove = &cs48l32_codec_remove,
+	.get_regmap = &cs48l32_get_regmap,
 
 	.idle_bias_off = true,
 
-	.set_sysclk = tacna_set_sysclk,
-	.set_pll = cs48l32_set_fll,
+	.set_sysclk = &tacna_set_sysclk,
+	.set_pll = &cs48l32_set_fll,
 
 	.component_driver = {
 		.controls = cs48l32_snd_controls,
@@ -1359,13 +1359,13 @@ static struct snd_soc_codec_driver soc_codec_dev_cs48l32 = {
 };
 
 static const struct snd_compr_ops cs48l32_compr_ops = {
-	.open = cs48l32_compr_open,
-	.free = wm_adsp_compr_free,
-	.set_params = wm_adsp_compr_set_params,
-	.get_caps = wm_adsp_compr_get_caps,
-	.trigger = wm_adsp_compr_trigger,
-	.pointer = wm_adsp_compr_pointer,
-	.copy = wm_adsp_compr_copy,
+	.open = &cs48l32_compr_open,
+	.free = &wm_adsp_compr_free,
+	.set_params = &wm_adsp_compr_set_params,
+	.get_caps = &wm_adsp_compr_get_caps,
+	.trigger = &wm_adsp_compr_trigger,
+	.pointer = &wm_adsp_compr_pointer,
+	.copy = &wm_adsp_compr_copy,
 };
 
 static const struct snd_soc_platform_driver cs48l32_compr_platform = {
@@ -1536,8 +1536,8 @@ static struct platform_driver cs48l32_codec_driver = {
 		.name = "cs48l32-codec",
 		.owner = THIS_MODULE,
 	},
-	.probe = cs48l32_probe,
-	.remove = cs48l32_remove,
+	.probe = &cs48l32_probe,
+	.remove = &cs48l32_remove,
 };
 
 module_platform_driver(cs48l32_codec_driver);

lucastanure/linux

sound/soc/codecs/cs48l32.c
@@ -1186,7 +1186,7 @@ static struct snd_soc_dai_driver cs48l32_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs48l32-dsp-trace",
@@ -1207,7 +1207,7 @@ static struct snd_soc_dai_driver cs48l32_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs48l32-dsp-voicectrl",
@@ -1310,25 +1310,25 @@ static int cs48l32_set_fll(struct snd_soc_component *comp, int fll_id, int sourc
 }
 
 static const struct snd_compr_ops cs48l32_compr_ops = {
-	.open = cs48l32_compr_open,
-	.free = wm_adsp_compr_free,
-	.set_params = wm_adsp_compr_set_params,
-	.get_caps = wm_adsp_compr_get_caps,
-	.trigger = wm_adsp_compr_trigger,
-	.pointer = wm_adsp_compr_pointer,
-	.copy = wm_adsp_compr_copy,
+	.open = &cs48l32_compr_open,
+	.free = &wm_adsp_compr_free,
+	.set_params = &wm_adsp_compr_set_params,
+	.get_caps = &wm_adsp_compr_get_caps,
+	.trigger = &wm_adsp_compr_trigger,
+	.pointer = &wm_adsp_compr_pointer,
+	.copy = &wm_adsp_compr_copy,
 };
 
 static struct snd_soc_component_driver soc_component_dev_cs48l32 = {
-	.probe = cs48l32_component_probe,
-	.remove = cs48l32_component_remove,
-	.compr_ops = cs48l32_compr_ops,
+	.probe = &cs48l32_component_probe,
+	.remove = &cs48l32_component_remove,
+	.compr_ops = &cs48l32_compr_ops,
 
 	.idle_bias_on = false,
 	.name		= DRV_NAME,
 
-	.set_sysclk = tacna_set_sysclk,
-	.set_pll = cs48l32_set_fll,
+	.set_sysclk = &tacna_set_sysclk,
+	.set_pll = &cs48l32_set_fll,
 
 	.controls = cs48l32_snd_controls,
 	.num_controls = ARRAY_SIZE(cs48l32_snd_controls),
@@ -1494,8 +1494,8 @@ static struct platform_driver cs48l32_component_driver = {
 		.name = "cs48l32-codec",
 		.owner = THIS_MODULE,
 	},
-	.probe = cs48l32_probe,
-	.remove = cs48l32_remove,
+	.probe = &cs48l32_probe,
+	.remove = &cs48l32_remove,
 };
 
 module_platform_driver(cs48l32_component_driver);