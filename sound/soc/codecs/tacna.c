CirrusLogic/linux-drivers

ASoC: tacna: Updates to new wm_adsp custom naming
Remove use of the suffix and dsp_ao members of struct wm_adsp,
these are replaced with the generic name member.
Add firmware selection controls to the codec driver controls.

Change-Id: I5edb55116b3b39a26f0f9a7f2d84013d4f79a033
Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

sound/soc/codecs/tacna.c
@@ -1992,8 +1992,8 @@ int tacna_dsp_rate_get(struct snd_kcontrol *kcontrol,
 	return 0;
 
 ovf_err:
-	dev_err(codec->dev, "DSP%u%s %cX rate control exceeds rate array\n",
-		dsp_num + 1, priv->dsp[dsp_num].suffix,
+	dev_err(codec->dev, "%s %cX rate control exceeds rate array\n",
+		priv->dsp[dsp_num].name,
 		(e->mask & TACNA_DSP_RATE_CTL_DIR_MASK) ? 'T' : 'R');
 
 	return -EINVAL;
@@ -2037,8 +2037,8 @@ int tacna_dsp_rate_put(struct snd_kcontrol *kcontrol,
 	return 0;
 
 ovf_err:
-	dev_err(codec->dev, "DSP%u%s %cX rate control exceeds rate array\n",
-		dsp_num + 1, priv->dsp[dsp_num].suffix,
+	dev_err(codec->dev, "%s %cX rate control exceeds rate array\n",
+		priv->dsp[dsp_num].name,
 		(e->mask & TACNA_DSP_RATE_CTL_DIR_MASK) ? 'T' : 'R');
 
 	return -EINVAL;

lucastanure/linux

sound/soc/codecs/tacna.c
@@ -1961,8 +1961,8 @@ int tacna_dsp_rate_get(struct snd_kcontrol *kcontrol,
 	return 0;
 
 ovf_err:
-	dev_err(codec->dev, "DSP%u%s %cX rate control exceeds rate array\n",
-		dsp_num + 1, priv->dsp[dsp_num].suffix,
+	dev_err(comp->dev, "%s %cX rate control exceeds rate array\n",
+		priv->dsp[dsp_num].name,
 		(e->mask & TACNA_DSP_RATE_CTL_DIR_MASK) ? 'T' : 'R');
 
 	return -EINVAL;
@@ -2006,8 +2006,8 @@ int tacna_dsp_rate_put(struct snd_kcontrol *kcontrol,
 	return 0;
 
 ovf_err:
-	dev_err(codec->dev, "DSP%u%s %cX rate control exceeds rate array\n",
-		dsp_num + 1, priv->dsp[dsp_num].suffix,
+	dev_err(comp->dev, "%s %cX rate control exceeds rate array\n",
+		priv->dsp[dsp_num].name,
 		(e->mask & TACNA_DSP_RATE_CTL_DIR_MASK) ? 'T' : 'R');
 
 	return -EINVAL;