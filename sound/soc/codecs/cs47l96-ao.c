CirrusLogic/linux-drivers

ASoC: cs47l96: Add & when getting address of function
Satisfies strict code checking tools.

Change-Id: I681c032e5b4c8663f1c2abbc0821dc9c284face2
Signed-off-by: Richard Fitzgerald <rf@opensource.cirrus.com>

sound/soc/codecs/cs47l96-ao.c
@@ -649,7 +649,7 @@ static struct snd_soc_dai_driver cs47l96_ao_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs47l96-ao-dsp-trace",
@@ -670,7 +670,7 @@ static struct snd_soc_dai_driver cs47l96_ao_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs47l96-ao-dsp-voicectrl",
@@ -913,14 +913,14 @@ static struct regmap *cs47l96_ao_get_regmap(struct device *dev)
 }
 
 static struct snd_soc_codec_driver soc_codec_dev_cs47l96_ao = {
-	.probe = cs47l96_ao_codec_probe,
-	.remove = cs47l96_ao_codec_remove,
-	.get_regmap = cs47l96_ao_get_regmap,
+	.probe = &cs47l96_ao_codec_probe,
+	.remove = &cs47l96_ao_codec_remove,
+	.get_regmap = &cs47l96_ao_get_regmap,
 
 	.idle_bias_off = true,
 
-	.set_sysclk = cs47l96_ao_set_sysclk,
-	.set_pll = cs47l96_ao_set_fll,
+	.set_sysclk = &cs47l96_ao_set_sysclk,
+	.set_pll = &cs47l96_ao_set_fll,
 
 	.component_driver = {
 		.controls = cs47l96_ao_snd_controls,
@@ -933,13 +933,13 @@ static struct snd_soc_codec_driver soc_codec_dev_cs47l96_ao = {
 };
 
 static const struct snd_compr_ops cs47l96_ao_compr_ops = {
-	.open = cs47l96_ao_compr_open,
-	.free = wm_adsp_compr_free,
-	.set_params = wm_adsp_compr_set_params,
-	.get_caps = wm_adsp_compr_get_caps,
-	.trigger = wm_adsp_compr_trigger,
-	.pointer = wm_adsp_compr_pointer,
-	.copy = wm_adsp_compr_copy,
+	.open = &cs47l96_ao_compr_open,
+	.free = &wm_adsp_compr_free,
+	.set_params = &wm_adsp_compr_set_params,
+	.get_caps = &wm_adsp_compr_get_caps,
+	.trigger = &wm_adsp_compr_trigger,
+	.pointer = &wm_adsp_compr_pointer,
+	.copy = &wm_adsp_compr_copy,
 };
 
 static const struct snd_soc_platform_driver cs47l96_ao_compr_platform = {
@@ -1101,8 +1101,8 @@ static struct platform_driver cs47l96_ao_codec_driver = {
 		.name = "cs47l96-ao-codec",
 		.owner = THIS_MODULE,
 	},
-	.probe = cs47l96_ao_probe,
-	.remove = cs47l96_ao_remove,
+	.probe = &cs47l96_ao_probe,
+	.remove = &cs47l96_ao_remove,
 };
 
 module_platform_driver(cs47l96_ao_codec_driver);

sound/soc/codecs/cs47l96.c
@@ -2615,7 +2615,7 @@ static struct snd_soc_dai_driver cs47l96_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs47l96-dsp-trace",
@@ -2789,14 +2789,14 @@ static struct regmap *cs47l96_get_regmap(struct device *dev)
 }
 
 static struct snd_soc_codec_driver soc_codec_dev_cs47l96 = {
-	.probe = cs47l96_codec_probe,
-	.remove = cs47l96_codec_remove,
-	.get_regmap = cs47l96_get_regmap,
+	.probe = &cs47l96_codec_probe,
+	.remove = &cs47l96_codec_remove,
+	.get_regmap = &cs47l96_get_regmap,
 
 	.idle_bias_off = true,
 
-	.set_sysclk = cs47l96_set_sysclk,
-	.set_pll = cs47l96_set_fll,
+	.set_sysclk = &cs47l96_set_sysclk,
+	.set_pll = &cs47l96_set_fll,
 
 	.component_driver = {
 		.controls = cs47l96_snd_controls,
@@ -2809,13 +2809,13 @@ static struct snd_soc_codec_driver soc_codec_dev_cs47l96 = {
 };
 
 static const struct snd_compr_ops cs47l96_compr_ops = {
-	.open = cs47l96_compr_open,
-	.free = wm_adsp_compr_free,
-	.set_params = wm_adsp_compr_set_params,
-	.get_caps = wm_adsp_compr_get_caps,
-	.trigger = wm_adsp_compr_trigger,
-	.pointer = wm_adsp_compr_pointer,
-	.copy = wm_adsp_compr_copy,
+	.open = &cs47l96_compr_open,
+	.free = &wm_adsp_compr_free,
+	.set_params = &wm_adsp_compr_set_params,
+	.get_caps = &wm_adsp_compr_get_caps,
+	.trigger = &wm_adsp_compr_trigger,
+	.pointer = &wm_adsp_compr_pointer,
+	.copy = &wm_adsp_compr_copy,
 };
 
 static const struct snd_soc_platform_driver cs47l96_compr_platform = {
@@ -2997,8 +2997,8 @@ static struct platform_driver cs47l96_codec_driver = {
 		.name = "cs47l96-codec",
 		.owner = THIS_MODULE,
 	},
-	.probe = cs47l96_probe,
-	.remove = cs47l96_remove,
+	.probe = &cs47l96_probe,
+	.remove = &cs47l96_remove,
 };
 
 module_platform_driver(cs47l96_codec_driver);

lucastanure/linux

sound/soc/codecs/cs47l96-ao.c
@@ -633,7 +633,7 @@ static struct snd_soc_dai_driver cs47l96_ao_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs47l96-ao-dsp-trace",
@@ -654,7 +654,7 @@ static struct snd_soc_dai_driver cs47l96_ao_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs47l96-ao-dsp-voicectrl",
@@ -883,25 +883,25 @@ static int cs47l96_ao_set_sysclk(struct snd_soc_component *comp, int clk_id,
 }
 
 static const struct snd_compr_ops cs47l96_ao_compr_ops = {
-	.open = cs47l96_ao_compr_open,
-	.free = wm_adsp_compr_free,
-	.set_params = wm_adsp_compr_set_params,
-	.get_caps = wm_adsp_compr_get_caps,
-	.trigger = wm_adsp_compr_trigger,
-	.pointer = wm_adsp_compr_pointer,
-	.copy = wm_adsp_compr_copy,
+	.open = &cs47l96_ao_compr_open,
+	.free = &wm_adsp_compr_free,
+	.set_params = &wm_adsp_compr_set_params,
+	.get_caps = &wm_adsp_compr_get_caps,
+	.trigger = &wm_adsp_compr_trigger,
+	.pointer = &wm_adsp_compr_pointer,
+	.copy = &wm_adsp_compr_copy,
 };
 
 static struct snd_soc_component_driver soc_component_dev_cs47l96_ao = {
-	.probe = cs47l96_ao_component_probe,
-	.remove = cs47l96_ao_component_remove,
+	.probe = &cs47l96_ao_component_probe,
+	.remove = &cs47l96_ao_component_remove,
 	.compr_ops = &cs47l96_ao_compr_ops,
 
 	.idle_bias_on = false,
 	.name		= DRV_NAME,
 
-	.set_sysclk = cs47l96_ao_set_sysclk,
-	.set_pll = cs47l96_ao_set_fll,
+	.set_sysclk = &cs47l96_ao_set_sysclk,
+	.set_pll = &cs47l96_ao_set_fll,
 
 	.controls = cs47l96_ao_snd_controls,
 	.num_controls = ARRAY_SIZE(cs47l96_ao_snd_controls),
@@ -1058,8 +1058,8 @@ static struct platform_driver cs47l96_ao_component_driver = {
 		.name = "cs47l96-ao-codec",
 		.owner = THIS_MODULE,
 	},
-	.probe = cs47l96_ao_probe,
-	.remove = cs47l96_ao_remove,
+	.probe = &cs47l96_ao_probe,
+	.remove = &cs47l96_ao_remove,
 };
 
 module_platform_driver(cs47l96_ao_component_driver);

sound/soc/codecs/cs47l96.c
@@ -2549,7 +2549,7 @@ static struct snd_soc_dai_driver cs47l96_dai[] = {
 			.rates = TACNA_RATES,
 			.formats = TACNA_FORMATS,
 		},
-		.compress_new = snd_soc_new_compress,
+		.compress_new = &snd_soc_new_compress,
 	},
 	{
 		.name = "cs47l96-dsp-trace",
@@ -2714,25 +2714,25 @@ static int cs47l96_set_sysclk(struct snd_soc_component *comp, int clk_id,
 }
 
 static const struct snd_compr_ops cs47l96_compr_ops = {
-	.open = cs47l96_compr_open,
-	.free = wm_adsp_compr_free,
-	.set_params = wm_adsp_compr_set_params,
-	.get_caps = wm_adsp_compr_get_caps,
-	.trigger = wm_adsp_compr_trigger,
-	.pointer = wm_adsp_compr_pointer,
-	.copy = wm_adsp_compr_copy,
+	.open = &cs47l96_compr_open,
+	.free = &wm_adsp_compr_free,
+	.set_params = &wm_adsp_compr_set_params,
+	.get_caps = &wm_adsp_compr_get_caps,
+	.trigger = &wm_adsp_compr_trigger,
+	.pointer = &wm_adsp_compr_pointer,
+	.copy = &wm_adsp_compr_copy,
 };
 
 static struct snd_soc_component_driver soc_component_dev_cs47l96 = {
-	.probe = cs47l96_component_probe,
-	.remove = cs47l96_component_remove,
-	.compr_ops = cs47l96_compr_ops,
+	.probe = &cs47l96_component_probe,
+	.remove = &cs47l96_component_remove,
+	.compr_ops = &cs47l96_compr_ops,
 
 	.idle_bias_on = false,
 	.name 	= DRV_NAME,
 
-	.set_sysclk = cs47l96_set_sysclk,
-	.set_pll = cs47l96_set_fll,
+	.set_sysclk = &cs47l96_set_sysclk,
+	.set_pll = &cs47l96_set_fll,
 
 	.controls = cs47l96_snd_controls,
 	.num_controls = ARRAY_SIZE(cs47l96_snd_controls),
@@ -2909,8 +2909,8 @@ static struct platform_driver cs47l96_component_driver = {
 		.name = "cs47l96-codec",
 		.owner = THIS_MODULE,
 	},
-	.probe = cs47l96_probe,
-	.remove = cs47l96_remove,
+	.probe = &cs47l96_probe,
+	.remove = &cs47l96_remove,
 };
 
 module_platform_driver(cs47l96_component_driver);