CirrusLogic/linux-drivers

ASoC: tacna: Make ao_dsp a member of struct wm_adsp
To avoid changing the existing API

Change-Id: I926118983af26ed706c246cf04e13d60b1908277
Signed-off-by: Lucas Tanure <tanureal@opensource.cirrus.com>

sound/soc/codecs/cs47l96-ao.c
@@ -747,7 +747,7 @@ static int cs47l96_ao_codec_probe(struct snd_soc_codec *codec)
 	if (ret)
 		return ret;
 
-	wm_adsp2_codec_probe(&cs47l96_ao->core.dsp[0], codec, true);
+	wm_adsp2_codec_probe(&cs47l96_ao->core.dsp[0], codec);
 
 	return 0;
 }
@@ -881,6 +881,7 @@ static int cs47l96_ao_probe(struct platform_device *pdev)
 	dsp->rev = 0;
 	dsp->dev = tacna->dev;
 	dsp->regmap = tacna->dsp_regmap[1];
+	dsp->ao_dsp = true;
 
 	dsp->base = TACNA_DSP2_CLOCK_FREQ;
 	dsp->base_sysinfo = TACNA_DSP2_SYS_INFO_ID;

sound/soc/codecs/cs47l96.c
@@ -2627,7 +2627,7 @@ static int cs47l96_codec_probe(struct snd_soc_codec *codec)
 	if (ret)
 		return ret;
 
-	wm_adsp2_codec_probe(&cs47l96->core.dsp[0], codec, false);
+	wm_adsp2_codec_probe(&cs47l96->core.dsp[0], codec);
 
 	return 0;
 }

sound/soc/codecs/cs48l32.c
@@ -1102,7 +1102,7 @@ static int cs48l32_codec_probe(struct snd_soc_codec *codec)
 	if (ret)
 		return ret;
 
-	wm_adsp2_codec_probe(&cs48l32->core.dsp[0], codec, false);
+	wm_adsp2_codec_probe(&cs48l32->core.dsp[0], codec);
 
 	return 0;
 }

lucastanure/linux

sound/soc/codecs/cs47l96-ao.c
@@ -752,7 +752,7 @@ static int cs47l96_ao_component_probe(struct snd_soc_component *comp)
 	if (ret)
 		return ret;
 
-	wm_adsp2_component_probe(&cs47l96_ao->core.dsp[0], comp, true);
+	wm_adsp2_component_probe(&cs47l96_ao->core.dsp[0], comp);
 
 	return 0;
 }
@@ -872,6 +872,7 @@ static int cs47l96_ao_probe(struct platform_device *pdev)
 	dsp->rev = 0;
 	dsp->dev = tacna->dev;
 	dsp->regmap = tacna->dsp_regmap[1];
+	dsp->ao_dsp = true;
 
 	dsp->base = TACNA_DSP2_CLOCK_FREQ;
 	dsp->base_sysinfo = TACNA_DSP2_SYS_INFO_ID;

sound/soc/codecs/cs47l96.c
@@ -2624,7 +2624,7 @@ static int cs47l96_component_probe(struct snd_soc_component *comp)
 	if (ret)
 		return ret;
 
-	wm_adsp2_component_probe(&cs47l96->core.dsp[0], comp, false);
+	wm_adsp2_component_probe(&cs47l96->core.dsp[0], comp);
 
 	return 0;
 }

sound/soc/codecs/cs48l32.c
@@ -1100,7 +1100,7 @@ static int cs48l32_component_probe(struct snd_soc_component *comp)
 	if (ret)
 		return ret;
 
-	wm_adsp2_component_probe(&cs48l32->core.dsp[0], comp, false);
+	wm_adsp2_component_probe(&cs48l32->core.dsp[0], comp);
 
 	return 0;
 }